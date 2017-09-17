20c411c78cd715e966094a9beebe9be7fd89c59a
SaiSandeep <saisandeep@saisandeeps-macbook-air.local>
2017-09-17 13:44:33 +0530

    Initial Commit

---
+++ BarCode&QRCodeTests/BarCode_QRCodeTests.swift
@@ -0,0 +1,36 @@
+//
+//  BarCode_QRCodeTests.swift
+//  BarCode&QRCodeTests
+//
+//  Created by SaiSandeep on 17/09/17.
+//  Copyright © 2017 iOSRevisited. All rights reserved.
+//
+
+import XCTest
+@testable import BarCode_QRCode
+
+class BarCode_QRCodeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
